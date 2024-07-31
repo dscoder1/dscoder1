330df47656575cd1d945041fdfd50787a410d913
dscoder1 <166431272+dscoder1@users.noreply.github.com>
2024-07-31 23:55:59 +0530

    Add files via upload

---
+++ FileOpen.c
@@ -0,0 +1,21 @@
+//WRITING TO A FILE
+/*#include<stdio.h>
+int main(){
+FILE *ptr=fopen("New.txt","w");
+char str[50]="My Name Is Divya Shakti";
+fputs(str,ptr);
+fclose(ptr);
+return 0;
+}
+
+// READING FROM FILE
+#include<stdio.h>
+int main(){
+FILE *ptr=fopen("New.txt","r");
+char str[50];
+while(fgets(str,2,ptr)!=NULL){
+printf("%s ",str);
+}
+return 0;
+}
+*/
